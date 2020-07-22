58ef7fe3d61b28554c55a69a9b976149b0569d5f
Amir <coloner12@gmail.com>
2020-07-22 08:43:04 -0700

    Registery completed

--- Registery.py
+++ Registery.py
@@ -6,38 +6,40 @@ class ServerRegistery:
 
     def __init__(self):
         Socket = socket.socket()
-        Socket.bind(('0.0.0.0', 12345))
+        Socket.bind(('0.0.0.0', 12468))
         Socket.listen(1)
         Client, Adder = Socket.accept()
-        print("Client with adder : " + Adder + " Connected !!!\n")
-        self.Menu(Socket)
+        print("Client with adder : " + str(Adder) + " Connected !!!\n")
+        self.Menu(Client)
+        Socket.close()
 
-    def Menu(self, Socket):
+    def Menu(self, Client):
 
             while (1) :
-                Socket.sendall("Welcome to Server Registery Serveice \n Please Enter your Username : \n".encode())
-                Username = Socket.recv(1024)
+                Client.sendall("Welcome to Server Registery Serveice \n Please Enter your Username : \n".encode())
+                Username = Client.recv(1024).decode()
+                print(Username)
                 if  self.UsernameCheck(Username) == 1:
                     break
                 else :
-                    Socket.sendall("This Username is already taken\n".encode())
+                    Client.sendall("This Username is already taken\n".encode())
                     time.sleep(3)
 
 
             flag = 1
             while (flag):
                 while (1) :
-                    Socket.sendall("Please Enter your Password : \n".encode())
-                    Password = Socket.recv(1024)
+                    Client.sendall("Please Enter your Password : \n".encode())
+                    Password = Client.recv(1024).decode()
                     if self.PassCheck(Username, Password) == 1 :
                         break
                     else :
-                        Socket.sendall("Password you have choosed is weak !!! Please choose anotherone\n".encode())
+                        Client.sendall("Password you have choosed is weak !!! Please choose anotherone\n".encode())
 
                 while (1) :
-                    Socket.sendall("Please re-enter your password : \n".encode())
-                    if Password == Socket.recv(1024):
-                        Socket.sendall("Registered !!!\n".encode())
+                    Client.sendall("Please re-enter your password : \n".encode())
+                    if Password == Client.recv(1024).decode():
+                        Client.sendall("Registered !!!\n".encode())
 
                         File = open ("users.txt", "r")
                         Salt = 1
@@ -49,13 +51,13 @@ class ServerRegistery:
                         File.close()
 
                         File = open("Users.txt", "a")
-                        File.write(Username + ":" + str(Salt) + ":" + Password)
+                        File.write(Username + ":" + str(Salt) + ":" + Password + "\n")
                         File.close()
 
                         flag = 0
                         break
                     else :
-                        Socket.sendall("Repeated Password is  not the same as Password !!!\n".encode())
+                        Client.sendall("Repeated Password is  not the same as Password !!!\n".encode())
                         break
             return 1
 
@@ -86,7 +88,7 @@ class ServerRegistery:
             UsernamePattern += char
             UsernamePattern += "(.*)"
 
-        if re.match(r'(.*)' + UsernamePattern, Password) :
+        if re.match(r'(.*)' + UsernamePattern, Password, re.I) :
             IsValid = -1
 
         if not re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', Password):
@@ -95,21 +97,21 @@ class ServerRegistery:
         return IsValid
 
 
-
 class ClientRegistery :
 
     def  __init__(self):
         Socket = socket.socket()
-        Socket.connect(('localhost', 12345))
+        Socket.connect(('localhost', 12468))
         self.Menu(Socket)
+        Socket.close()
 
     def Menu (self, Socket) :
 
         while (1):
-            print(Socket.recv(1024))
+            print(Socket.recv(1024).decode())
             Username = input()
             Socket.sendall(Username.encode())
-            if Socket.recv(1024) == "Please Enter your Password : \n" :
+            if Socket.recv(1024).decode() == "Please Enter your Password : \n" :
                 break
             else :
                 print("This Username is already taken\n")
@@ -120,7 +122,7 @@ class ClientRegistery :
                 print("Please Enter your Password : \n")
                 Password = input()
                 Socket.sendall(Password.encode())
-                if Socket.recv(1024) == "Please re-enter your password : \n" :
+                if Socket.recv(1024).decode() == "Please re-enter your password : \n" :
                     break
                 else :
                     print("Password you have choosed is weak !!! Please choose anotherone\n")
@@ -129,7 +131,7 @@ class ClientRegistery :
                 print("Please re-enter your password : \n")
                 Password = input()
                 Socket.sendall(Password.encode())
-                if Socket.recv(1024) == "Registered !!!\n" :
+                if Socket.recv(1024).decode() == "Registered !!!\n" :
                     print("Registered !!!\n")
                     flag = 0
                     break
@@ -137,7 +139,3 @@ class ClientRegistery :
                     print("Repeated Password is  not the same as Password !!!\n")
                     break
         return 1
-
-
-p = ServerRegistery()
-print(p.Menu())
